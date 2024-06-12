55f4d9eb49fe08448364f6569a3b2e8dcc5c1aec
sangeun-b <baek0007@algonquinlive.com>
2024-06-12 14:31:32 +0900

    L9 답

---
+++ components/ProductDetails.js
@@ -0,0 +1,15 @@
+app.component('product-details', {
+    prop: {
+        deatils: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /* html */
+    `
+    <ul>
+        <li v-for="detail in details"> {{ detail }}</li>
+    </ul>
+    `
+})
